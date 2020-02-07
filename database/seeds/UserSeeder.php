10ac6af9d154292ec43b3f30b603a002ffd369ec
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-02-08 00:10:01 +0800

    spatie installation

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->firstname = "john kevin";
+        $user->middlename = "pama";
+        $user->lastname = "paunel";
+        $user->mobileNo = "09166520817";
+        $user->date_of_birth = "09/09/1990";
+        $user->email = "johnkevinpaunel@gmail.com";
+        $user->username = "kevinpauneljohn";
+        $user->password = bcrypt("123");
+        $user->assignRole('super admin');
+        $user->save();
+    }
+}
