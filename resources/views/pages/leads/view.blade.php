6b0e9b0967b127503562e931db83d83ce6e1829e
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-02-23 20:54:26 +0800

    spatie activity log installation

--- resources/views/pages/leads/view.blade.php
+++ resources/views/pages/leads/view.blade.php
@@ -26,26 +26,16 @@
                 <div class="card card-primary card-outline">
                     <div class="card-body box-profile">
                         <div class="text-center">
-                            <img class="profile-user-img img-fluid img-circle" src="../../dist/img/user4-128x128.jpg" alt="User profile picture">
+                            <img class="profile-user-img img-fluid img-circle" src="{{asset('/images/avatar.png')}}" alt="User profile picture">
                         </div>
 
-                        <h3 class="profile-username text-center">Nina Mcintire</h3>
+                        <h3 class="profile-username text-center">
+                            {{ucfirst($lead->firstname)}}
+                            {{ucfirst($lead->middlename)}}
+                            {{ucfirst($lead->lastname)}}
+                        </h3>
 
-                        <p class="text-muted text-center">Software Engineer</p>
-
-                        <ul class="list-group list-group-unbordered mb-3">
-                            <li class="list-group-item">
-                                <b>Followers</b> <a class="float-right">1,322</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Following</b> <a class="float-right">543</a>
-                            </li>
-                            <li class="list-group-item">
-                                <b>Friends</b> <a class="float-right">13,287</a>
-                            </li>
-                        </ul>
-
-                        <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+                        <a href="{{route('leads.show',['lead' => $lead->id])}}" class="btn btn-primary btn-block"><b>Edit</b></a>
                     </div>
                     <!-- /.card-body -->
                 </div>
@@ -54,39 +44,59 @@
                 <!-- About Me Box -->
                 <div class="card card-primary">
                     <div class="card-header">
-                        <h3 class="card-title">About Me</h3>
+                        <h3 class="card-title">Details</h3>
                     </div>
                     <!-- /.card-header -->
                     <div class="card-body">
-                        <strong><i class="fas fa-book mr-1"></i> Education</strong>
+                        <strong><i class="fas fa-map-marker-alt mr-1"></i> Address</strong>
+
+                        <p class="text-muted">{{$lead->address}}</p>
+
+                        <hr>
+
+                        <strong><i class="fas fa-phone mr-1"></i> Landline</strong>
 
                         <p class="text-muted">
-                            B.S. in Computer Science from the University of Tennessee at Knoxville
+                            {{$lead->landline}}
                         </p>
 
                         <hr>
 
-                        <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
-
-                        <p class="text-muted">Malibu, California</p>
-
-                        <hr>
-
-                        <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+                        <strong><i class="fas fa-phone mr-1"></i> Mobile Phone</strong>
 
                         <p class="text-muted">
-                            <span class="tag tag-danger">UI Design</span>
-                            <span class="tag tag-success">Coding</span>
-                            <span class="tag tag-info">Javascript</span>
-                            <span class="tag tag-warning">PHP</span>
-                            <span class="tag tag-primary">Node.js</span>
+                            {{$lead->mobileNo}}
                         </p>
 
                         <hr>
 
-                        <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+                        <strong><i class="fas fa-mail-forward mr-1"></i> Email</strong>
 
-                        <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+                        <p class="text-muted">
+                            {{$lead->email}}
+                        </p>
+
+                        <hr>
+
+                        <strong><i class="fas fa-money-bill mr-1"></i> Income Range</strong>
+
+                        <p class="text-muted">
+                            {{$lead->income_range}}
+                        </p>
+
+                        <strong><i class="fas fa-phone mr-1"></i> Point Of Contact</strong>
+
+                        <p class="text-muted">
+                            {{$lead->point_of_contact}}
+                        </p>
+
+                        <hr>
+
+                        <hr>
+
+                        <strong><i class="far fa-file-alt mr-1"></i> Remarks</strong>
+
+                        <p class="text-muted">{!! $lead->remarks !!}</p>
                     </div>
                     <!-- /.card-body -->
                 </div>
@@ -251,7 +261,7 @@
                                     </div>
                                     <!-- END timeline item -->
                                     <!-- timeline item -->
-                                    <div>
+                                    <div>Nina Mcintire
                                         <i class="fas fa-user bg-info"></i>
 
                                         <div class="timeline-item">
