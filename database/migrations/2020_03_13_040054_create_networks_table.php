52ab3cb2550b07a803055b8804847ef7629770e4
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-03-13 12:05:21 +0800

    Sales module

---
+++ database/migrations/2020_03_13_040054_create_networks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNetworksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('networks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('user_id');
+            $table->uuid('upline_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('upline_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('networks');
+    }
+}
