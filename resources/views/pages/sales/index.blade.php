79542c1690f716eb1caa94e6afa9b283c62e3788
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-03-31 01:42:56 +0800

    change password

--- resources/views/pages/sales/index.blade.php
+++ resources/views/pages/sales/index.blade.php
@@ -22,7 +22,7 @@
             <!-- small box -->
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>{{$total_sales}}</h3>
+                    <h3>&#8369; {{$total_sales}}</h3>
 
                     <p>Total Sales</p>
                 </div>
