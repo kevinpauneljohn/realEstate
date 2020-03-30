b31ac5df970eeaffb32caa3b55a40fbc802cc9a3
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-03-31 01:13:01 +0800

    change password

--- resources/views/pages/sales/index.blade.php
+++ resources/views/pages/sales/index.blade.php
@@ -135,7 +135,7 @@
                         </div>
                         <div class="modal-body">
                             <div class="image-loader">
-                                <div class="spinner-border text-secondary" role="status">
+                                <div class="spinner-border text-primary" role="status">
                                     <span class="sr-only">Loading...</span>
                                 </div>
                             </div>
