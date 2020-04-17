b97f34872f371321ee03beaee17545b8b1f0e928
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-04-17 21:27:42 +0800

    action module

--- routes/web.php
+++ routes/web.php
@@ -108,7 +108,7 @@ Route::get('/upline-commission/{project}','CommissionController@getUpLineCommiss
 
 Route::get('/test',function(\Illuminate\Http\Request $request){
 
-    return $request->all();
+    return 'kevin';
 });
 
 /*change password*/
