049995536596becab214a3750b0339ec5a8adea9
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-05-17 14:34:08 +0800

    canned message - view module

--- resources/views/pages/canned/addCanned.blade.php
+++ resources/views/pages/canned/addCanned.blade.php
@@ -155,7 +155,7 @@
                         </div>
                         <div class="modal-footer justify-content-between">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="submit" class="btn btn-primary submit-form-btn"><i class="spinner fa fa-spinner fa-spin"></i> Save</button>
+                            <input type="submit" class="btn btn-primary submit-form-btn" value="Save">
                         </div>
                     </div>
                 </div>
