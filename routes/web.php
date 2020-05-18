bc55418914c2a3e1a36cf7b69c2c803772da291c
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-05-18 18:11:30 +0800

    canned message - view module

--- routes/web.php
+++ routes/web.php
@@ -162,6 +162,9 @@ Route::get('/priorities/list','PriorityController@priorityList')->name('prioriti
 Route::post('/priorities','PriorityController@store')->name('priorities.store')->middleware(['auth','permission:add priority']);
 Route::put('/priorities/{priority}','PriorityController@update')->name('priorities.update')->middleware(['auth','permission:edit priority']);
 Route::delete('/priorities/{priority}','PriorityController@destroy')->name('priorities.delete')->middleware(['auth','permission:delete priority']);
+Route::get('/update-priority-status',function(){
+    \Illuminate\Support\Facades\Artisan::call('update:priority');
+});
 
 /*actions*/
 Route::get('/actions','ActionController@index')->name('actions.index')->middleware(['auth','permission:view action']);
