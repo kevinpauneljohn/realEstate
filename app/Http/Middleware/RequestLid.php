9146262ee795f5d7c11a23deecda021fe06c427e
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-05-24 16:07:07 +0800

    added threshold notification using pusher

--- app/Http/Middleware/RequestLid.php
+++ app/Http/Middleware/RequestLid.php
@@ -25,7 +25,7 @@ class RequestLid
         if($threshold->user_id !== auth()->user()->id && $threshold->lid === 0)
         {
             //you dont have any access in this page
-            return response("You must open the lid before you access the content!",401)->header('Content-Type', 'text/plain');
+            return response("You must open the lid before you access the content!<br/><a href='".route('thresholds.index')."'>View all request</a>",401)->header('Content-Type', 'text/html');
         }
 
         return $response;
