c422c3dd86a8607c05c40994c58f708918ef26e2
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-05-28 12:30:55 +0800

    computations module

--- resources/views/pages/requirements/index.blade.php
+++ resources/views/pages/requirements/index.blade.php
@@ -113,7 +113,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h4 class="modal-title">Update Project</h4>
+                            <h4 class="modal-title">Update Requirements</h4>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">×</span>
                             </button>
