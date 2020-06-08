7fb3e1cc438d26843bde73e151bcb003bf6818c3
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-06-08 23:39:45 +0800

    rank module

--- resources/views/pages/rank/index.blade.php
+++ resources/views/pages/rank/index.blade.php
@@ -197,7 +197,7 @@
                         { data: 'action', name: 'action', orderable: false, searchable: false}
                     ],
                     responsive:true,
-                    order:[0,'asc']
+                    order:[2,'asc']
                 });
             });
         </script>
