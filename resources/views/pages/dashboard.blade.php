5d4f50b48be26043a457b2df3ec9a89d28e8913a
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-06-11 21:20:28 +0800

    rank module

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -78,35 +78,48 @@
             </div>
         </div>
         <div class="col-lg-4">
-            <div class="card">
-                <div class="card-body">
-                    <h6>You have <span class="text-danger">{{$reminders->count()}}</span> unread @if($reminders->count() > 1) reminders @else reminder @endif</h6>
-                    <div class="row">
-                        <div class="col-12">
-                            <table class="table">
-                                @foreach($reminders->limit(3)->get() as $remind)
-                                    <tr>
-                                        <td>
-                                            <a href="{{route('leads.show',['lead' => $remind->data->lead_id])}}">
-                                            <div class="media">
-                                                <img src="http://crm.dream-homeseller.com/images/avatar-sm.png" class="user-image img-circle elevation-2" height="40" style="margin:0px 10px 10px 10px;">
-                                                <div class="media-body">
-                                                    <h3 class="dropdown-item-title">
-                                                        {{$remind->data->category}} to <span class="text-muted">{{ucfirst($remind->data->client_name)}}</span>
-                                                        <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> {{$remind->data->time_left}}</p>
-                                                    </h3>
-                                                </div>
-                                            </div>
-                                            </a>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                            </table>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h2 class="text-muted" align="center">Ranking Coming Soon</h2>
                         </div>
                     </div>
                 </div>
-                <div class="card-footer" align="center">
-                    <a href="{{route('notifications.index')}}" class="btn btn-default btn-sm">View All Reminders</a>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6>You have <span class="text-danger">{{$reminders->count()}}</span> unread @if($reminders->count() > 1) reminders @else reminder @endif</h6>
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table">
+                                        @foreach($reminders->limit(3)->get() as $remind)
+                                            <tr>
+                                                <td>
+                                                    <a href="{{route('leads.show',['lead' => $remind->data->lead_id])}}">
+                                                        <div class="media">
+                                                            <img src="http://crm.dream-homeseller.com/images/avatar-sm.png" class="user-image img-circle elevation-2" height="40" style="margin:0px 10px 10px 10px;">
+                                                            <div class="media-body">
+                                                                <h3 class="dropdown-item-title">
+                                                                    {{$remind->data->category}} to <span class="text-muted">{{ucfirst($remind->data->client_name)}}</span>
+                                                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> {{$remind->data->time_left}}</p>
+                                                                </h3>
+                                                            </div>
+                                                        </div>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer" align="center">
+                            <a href="{{route('notifications.index')}}" class="btn btn-default btn-sm">View All Reminders</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
