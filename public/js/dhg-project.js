033b130ae5a778e511f74674110e7ca8c867ae85
kevinpauneljohn <johnkevinpaunel@gmail.com>
2020-12-14 23:11:50 +0800

    DHG projects module

--- public/js/dhg-project.js
+++ public/js/dhg-project.js
@@ -14,7 +14,6 @@ $(document).on('submit','#add-project-form',function(form){
     form.preventDefault();
 
     let data = $(this).serializeArray();
-    //console.log(data);
 
     $.ajax({
         'url' : '/dhg-projects',
@@ -23,7 +22,6 @@ $(document).on('submit','#add-project-form',function(form){
         beforeSend: function(){
             $('.dhg-project-form-btn').attr('disabled',true).val('Saving...');
         },success: function(result){
-            console.log(result);
             if(result.success === true)
             {
                 $('#add-project-form').trigger('reset');
@@ -53,7 +51,6 @@ $(document).on('submit','#add-project-form',function(form){
 
 $(document).on('click','.edit-btn', function(){
     rowId = this.id;
-    //console.log(rowId);
 
     $.ajax({
         'url' : '/dhg-projects/'+rowId+'/edit',
@@ -61,7 +58,6 @@ $(document).on('click','.edit-btn', function(){
         beforeSend: function(){
             $('#edit-project-form input, #edit-project-form select, #edit-project-form textarea').attr('disabled',true);
         },success: function(result){
-            console.log(result);
 
             $('#edit_client').val(result.user_id).change();
             $('#edit_architect').val(result.architect_id).change();
