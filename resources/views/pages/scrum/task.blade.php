3715695720906ef109b714df5ed49c412c80ab39
kevinpauneljohn <johnkevinpaunel@gmail.com>
2021-03-17 16:16:52 +0800

    task module and checklist module

--- resources/views/pages/scrum/task.blade.php
+++ resources/views/pages/scrum/task.blade.php
@@ -218,11 +218,11 @@
                 });
             });
 
-        $(document).on('submit','#edit-task-form',function(form){
-            form.preventDefault();
-
-            let formData = $(this).serialize();
-            console.log('test '+formData);
+        // $(document).on('submit','#edit-task-form',function(form){
+        //     form.preventDefault();
+        //
+        //     let formData = $(this).serialize();
+        //     console.log('test '+formData);
             // $.ajax({
             //     'url' : '/tasks/'+data[0].value,
             //     'type' : 'PUT',
@@ -255,8 +255,47 @@
             //     }
             // });
             // clear_errors('title','description','due_date','priority','assign_to');
-        });
+        // });
         @endcan
+
+        // $(document).on('submit','#task-form',function(form){
+        //     form.preventDefault();
+        //
+        //     let data = $(this).serializeArray();
+        //
+        //     $.ajax({
+        //         'url' : '/tasks',
+        //         'type' : 'POST',
+        //         'data' : data,
+        //         beforeSend: function(){
+        //             $('.submit-task-btn').val('Saving ...').attr('disabled',true);
+        //         },success: function(result){
+        //
+        //             if(result.success === true)
+        //             {
+        //                 let table = $('#task-list').DataTable();
+        //                 table.ajax.reload();
+        //                 $('#task-form').trigger('reset');
+        //                 $('#task-form #collaborator').empty();
+        //                 toastr.success(result.message);
+        //
+        //                 $('#add-task-modal').modal('toggle');
+        //                 $('#assign_to, #priority').val([]).trigger('change');
+        //             }
+        //
+        //             $.each(result, function (key, value) {
+        //                 let element = $('.'+key);
+        //
+        //                 element.find('.error-'+key).remove();
+        //                 element.append('<p class="text-danger error-'+key+'">'+value+'</p>');
+        //             });
+        //             $('.submit-task-btn').val('Save').attr('disabled',false);
+        //         },error: function(xhr, status, error){
+        //             console.log(xhr);
+        //         }
+        //     });
+        //     clear_errors('title','description','due_date','priority','assign_to');
+        // });
     </script>
 @stop
 
