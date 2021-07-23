b941680686a2c46c8108b3875b7c4e9557185392
kevinpauneljohn <johnkevinpaunel@gmail.com>
2021-07-24 07:17:54 +0800

    setup the payment reminder

--- resources/views/emails/myTestMail.blade.php
+++ resources/views/emails/myTestMail.blade.php
@@ -9,7 +9,7 @@
     Hi {{$reminder->sales->lead->fullname}},<br/>
 
     This is to remind you regarding your monthly payment amounting {{$reminder->amount}}
-    on {{\Carbon\Carbon::create($reminder->schedule)->format('Y-M-d')}} for the project you reserved at {{$reminder->sales->lead->project->name}}, model unit {{$reminder->sales->modelUnit->name}}
+    on {{\Carbon\Carbon::create($reminder->schedule)->format('Y-M-d')}} for the project you reserved at {{$reminder->sales->project->name}}, model unit {{$reminder->sales->modelUnit->name}}
 </p>
 </body>
 </html>
