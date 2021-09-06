cc13087c2455903133b8b844cc01042713bd25f5
kevinpauneljohn <johnkevinpaunel@gmail.com>
2021-09-06 20:58:50 +0800

    leads controller api for openhouseseller.com

--- app/Http/Controllers/OpenHouseSeller/LeadsApiController.php
+++ app/Http/Controllers/OpenHouseSeller/LeadsApiController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\OpenHouseSeller;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class LeadsController extends Controller
+class LeadsApiController extends Controller
 {
     /**
      * Display a listing of the resource.
