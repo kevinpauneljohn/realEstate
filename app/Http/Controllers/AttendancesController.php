161a9805778523ebb19602d3a4ef19f6c9c71e59
IsarNaira <ccis.ezrajoshua.pacete@gmail.com>
2022-10-12 20:56:16 +0800

    index and timesheet page

--- app/Http/Controllers/AttendancesController.php
+++ app/Http/Controllers/AttendancesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\DataTables;
 
 class AttendancesController extends Controller
 {
--- resources/views/pages/attendances/timesheet.blade.php
+++ resources/views/pages/attendances/timesheet.blade.php
@@ -1,20 +1,113 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Timesheet</title></title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+@extends('adminlte::page')
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      <h1>Timesheet</h1>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
+@section('title', 'Timesheet')
+
+@section('content_header')
+    <div class="row mb-2">
+        <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Timesheet</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+                <li class="breadcrumb-item"><a href="{{route('dashboard')}}">Home</a></li>
+                <li class="breadcrumb-item active">Timesheet</li>
+            </ol>
+        </div><!-- /.col -->
+    </div>
+@stop
+
+@section('content')
+    <div class="container-fluid">
+        <div class="card">
+            <div class="card-header">
+
+
+            </div>
+            <div class="card-body">
+                <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
+                    <table id="commission-request-list" class="table table-bordered table-hover" role="grid">
+                        <thead>
+                        <tr role="row">
+                            <th>Request #</th>
+                            <th>Date Requested</th>
+                            <th>Project</th>
+                            <th>Client</th>
+                            <th>TCP</th>
+                            <th>Discount</th>
+                            <th>Agent</th>
+                            <th>Up Line</th>
+                            <th>Comm. Rate</th>
+                            <th>Rate Requested</th>
+                            <th>Last Due Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+
+                        <tfoot>
+                        <tr>
+                            <th>Request #</th>
+                            <th>Date Requested</th>
+                            <th>Project</th>
+                            <th>Client</th>
+                            <th>TCP</th>
+                            <th>Discount</th>
+                            <th>Agent</th>
+                            <th>Up Line</th>
+                            <th>Comm. Rate</th>
+                            <th>Rate Requested</th>
+                            <th>Last Due Date</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@stop
+
+@section('css')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="stylesheet" href="{{asset('/css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('vendor/datatables/css/dataTables.bootstrap4.min.css')}}">
+    <style type="text/css">
+
+    </style>
+@stop
+
+@section('js')
+        <script src="{{asset('vendor/datatables/js/dataTables.bootstrap4.min.js')}}"></script>
+        @can('view commission request')
+            <script>
+                $(function() {
+                    $('#commission-request-list').DataTable({
+                        processing: true,
+                        serverSide: true,
+                        ajax: '{!! route('commission.request.approval.get') !!}',
+                        columns: [
+                            { data: 'requestNo', name: 'requestNo'},
+                            { data: 'dateRequested', name: 'dateRequested'},
+                            { data: 'project', name: 'project'},
+                            { data: 'client', name: 'client'},
+                            { data: 'tcp', name: 'tcp'},
+                            { data: 'discount', name: 'discount'},
+                            { data: 'agent', name: 'agent'},
+                            { data: 'upLine', name: 'upLine'},
+                            { data: 'rate', name: 'rate'},
+                            { data: 'rateRequested', name: 'rateRequested'},
+                            { data: 'lastDueDate', name: 'lastDueDate'},
+                            { data: 'status', name: 'status'},
+                            { data: 'action', name: 'action', orderable: false, searchable: false}
+                        ],
+                        responsive:true,
+                        order:[0,'asc']
+                    });
+                });
+                //Initialize Select2 Elements
+                $('.select2').select2();
+            </script>
+        @endcan
+@stop
