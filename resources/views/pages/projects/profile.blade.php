c0af169ca793150e9226a41523633574a23f60e1
kevinpauneljohn <johnkevinpaunel@gmail.com>
2024-07-02 18:00:41 +0800

    files

--- resources/views/pages/projects/profile.blade.php
+++ resources/views/pages/projects/profile.blade.php
@@ -30,7 +30,7 @@
                     @endcan
                 </div>
                 <div class="card-body">
-                    <a href="fb://facewebmodal/f?href=https://www.facebook.com/messages/t/4431281110315860">Open FB</a>
+                    <a href="fb://facewebmodal/f?href=https://it-it.facebook.com/messages/t/4431281110315860">Open FB</a>
 
                     <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                         <table id="model-units-list" class="table table-bordered table-striped" role="grid">
