bf936671644b6930190f7e8038ce546a3572b9bc
kevinpauneljohn <johnkevinpaunel@gmail.com>
2025-05-22 11:09:02 +0800

    update the lists of agents

--- app/Http/Controllers/ScrumController.php
+++ app/Http/Controllers/ScrumController.php
@@ -33,7 +33,7 @@ class ScrumController extends Controller
     )
     {
         $this->task = $task;
-        $this->agents = ['admin','account manager','online warrior','super admin','agent','dhg_ojt'];
+        $this->agents = ['admin','account manager','online warrior','super admin','vide editor','dhg_ojt','Finance Admin','programmer'];
     }
     public function index()
     {
